30e816f2342ca1ddc154a01e566af5c1d3dde0b3
R. ALOUI <aloui.radhouane@gmail.com>
2024-07-08 20:16:16 +0100

    Initial nema23 working prg

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,23 @@
 #include <Arduino.h>
 
-void setup(){
-  Serial.begin(9600);
-  Serial.println("Bonjour");
+const int ena = 2;          // habilita o motor
+const int dir = 3;          // determina a direção
+const int pul = 4;          // executa um passo
+const int intervalo = 350;  // intervalo entre as
+                            //  mudanças de estado do pulso
+boolean pulso = LOW;  // estado do pulso
+
+void setup() {
+    pinMode(ena, OUTPUT);
+    pinMode(dir, OUTPUT);
+    pinMode(pul, OUTPUT);
+    digitalWrite(ena, LOW);   // habilita em low invertida
+    digitalWrite(dir, HIGH);  // low CW / high CCW
+    digitalWrite(pul, HIGH);  // borda de descida
 }
-void loop(){}
+
+void loop() {
+    pulso = !pulso;            // inverte o estado da variável
+    digitalWrite(pul, pulso);  // atribui o novo estado à porta
+    delayMicroseconds(intervalo);
+}
