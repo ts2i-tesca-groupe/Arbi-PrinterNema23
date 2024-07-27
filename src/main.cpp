571e484582968a1eb2d731a358084a4a3ab642af
R. ALOUI <aloui.radhouane@gmail.com>
2024-07-27 08:10:26 +0100

    Use fncallback with correct maxSpeed mapping

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,38 @@
 #include <Arduino.h>
 #include <MagicPot.h>
 
-#define DEBUG_BAUDIOS 9600
-#define POTENTIOMETER_PIN A0
+#define DEBUG_BAUDIOS       115200
+#define POTENTIOMETER_PIN   A1
+#define MIN_VAL_CHANGE      5
+#define MAX_SPEED           500000
 
-MagicPot potentiometer(POTENTIOMETER_PIN);
+MagicPot potentiometer(POTENTIOMETER_PIN, 0, MAX_SPEED, MAX_RAW_VALUE_DEFAULT);
+
+uint16_t oldSpeed = 0;
+void speedUpdate(){
+    if (abs(potentiometer.getRawValue() - oldSpeed) > MIN_VAL_CHANGE){
+        /* update speed */
+        oldSpeed = potentiometer.getRawValue();
+    }
+    
+}
 
 void setup()
 {
 	Serial.begin(DEBUG_BAUDIOS);
 
 	potentiometer.begin();
+
+    Serial.println(String("Testing ... ") + map(940, 0, 1023, 0, 500000));
 }
 
 void loop()
 {
 	potentiometer.read();
+    
+    Serial.println(String("Testing ... ") + map(940, 0, 1023, 0, 500000));
+	//Serial.println(String("Raw value: ") + potentiometer.getRawValue()
+    //    + String(" value: ") + potentiometer.getValue());
 
-	Serial.println(String("Raw value: ") + potentiometer.getRawValue());
+    delay(1000);
 }
